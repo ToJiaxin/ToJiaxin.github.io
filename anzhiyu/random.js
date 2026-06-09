2d279004981c12003b85795e9bdcd0c911581854
SkyEleven <2471736500@qq.com>
2026-06-10 01:21:30 +0800

    Site updated: 2026-06-10 01:21:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/28/hello-world/"];function toRandomPost(){
+var posts=["2025/04/28/hello-world/","2025/07/16/一天半夜突然有很多想说的话/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
